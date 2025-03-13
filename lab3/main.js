864a7629ab0f9462e7e9dbf6d7c3fbaa2119203a
Wojciech Pietrzak <wojciechpietrzak2003@gmail.com>
2025-03-13 18:15:36 +0100

    a

--- lab3/main.js
+++ lab3/main.js
@@ -4,6 +4,8 @@ const clapSound = document.querySelector('#clap')
 const hihatSound = document.querySelector('#hihat')
 const kickSound = document.querySelector('#kick')
 
+const btnRecord = document.querySelector('#record')
+
 const sounds = {
     a: clapSound,
     s: hihatSound,
@@ -25,3 +27,11 @@ function play(sound){
     sound.currentTime = 0;
     sound.play();
 }
+
+btnRecord.addEventListener('click', () =>{
+    if(btnRecord.innerText = 'Start Recording'){
+        btnRecord.innerText = 'Stop Recording'
+    }if(btnRecord.innerText = 'Stop Recording'){
+        btnRecord.innerText = 'Start Recording'
+    }
+})
