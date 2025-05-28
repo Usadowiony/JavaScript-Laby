17d21d80bd5d33d81c0b68f1e1a33580d43bba4c
Usadowiony <wojciechpietrzak2003@gmail.com>
2025-05-28 17:05:18 +0200

    add api fetch

---
+++ lab8/main.js
@@ -0,0 +1,15 @@
+function getWeather(city) {
+    const apiKey = '5a5f4b7ff8003dfe76f9c9448a301526';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pl`;
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Nie znaleziono miasta lub błąd sieci!');
+            }
+            return response.json();
+        })
+}
+
+getWeather('Warszawa').then(data => {
+    console.log(data); // tu masz dane pogodowe dla Warszawy
+});
