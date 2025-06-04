c6268d80eded812b16c3ea1ae79917d1953f3ea9
Usadowiony <wojciechpietrzak2003@gmail.com>
2025-06-04 11:16:42 +0200

    commit

--- lab7/main.js
+++ lab7/main.js
@@ -2,15 +2,20 @@ const gameArea = document.getElementById('game-area');
 const canvasArea = document.createElement('canvas');
 canvasArea.width = 600;
 canvasArea.height = 1000;
-gameArea.appendChild(canvasArea); // poprawiona literówka
+gameArea.appendChild(canvasArea);
 
-const ctx = canvasArea.getContext('2d');
+const ctx = canvasArea.getContext('2d'); //Context do rysowania na canvasie
+let ballX = 30;
+let ballY = 30;
 
-ctx.beginPath();
-ctx.arc(30, 30, 30, 0, Math.PI * 2); // Narysuj okrąg
-ctx.fillStyle = 'blue';              // Ustaw kolor
-ctx.fill();                          // Wypełnij okrąg kolorem
-ctx.closePath();
+function drawBall(){
+    ctx.clearRect(0, 0, canvasArea.width, canvasArea.height); // czyść canvas
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, 30, 0, Math.PI * 2); // Narysuj okrąg
+    ctx.fillStyle = 'blue';              // Ustaw kolor
+    ctx.fill();                          // Wypełnij okrąg kolorem
+    ctx.closePath();
+}
 
 window.addEventListener('deviceorientation', (event) =>{
     let alphaValue = event.alpha.toFixed(0); // Pobierz wartość alpha i zaokrąglij do pełnych stopni
