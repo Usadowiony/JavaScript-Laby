7c1f84914a11f32848e142e7ac7700cd02004074
Usadowiony <wojciechpietrzak2003@gmail.com>
2025-06-04 11:00:13 +0200

    work

--- lab7/main.js
+++ lab7/main.js
@@ -7,7 +7,12 @@ gameArea.appendChild(canvasArea); // poprawiona literówka
 const ctx = canvasArea.getContext('2d');
 
 ctx.beginPath();
-ctx.arc(100, 100, 30, 0, Math.PI * 2); // Narysuj okrąg
+ctx.arc(30, 30, 30, 0, Math.PI * 2); // Narysuj okrąg
 ctx.fillStyle = 'blue';              // Ustaw kolor
 ctx.fill();                          // Wypełnij okrąg kolorem
 ctx.closePath();
+
+window.addEventListener('deviceorientation', (event) =>{
+    let alphaValue = event.alpha.toFixed(0); // Pobierz wartość alpha i zaokrąglij do pełnych stopni
+    console.log(alphaValue)
+});
